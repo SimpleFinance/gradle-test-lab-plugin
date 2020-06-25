b3591c2251b94675d040feeaad24c66afab19282
Tad Fisher <tad@simple.com>
2020-06-25 14:25:39 -0700

    Fix kotlin format

--- test-lab-plugin/src/main/kotlin/com/simple/gradle/testlab/tasks/TestLabTest.kt
+++ test-lab-plugin/src/main/kotlin/com/simple/gradle/testlab/tasks/TestLabTest.kt
@@ -29,7 +29,6 @@ import org.gradle.api.model.ObjectFactory
 import org.gradle.api.provider.Property
 import org.gradle.api.tasks.Input
 import org.gradle.api.tasks.InputFile
-import org.gradle.api.tasks.Internal
 import org.gradle.api.tasks.Optional
 import org.gradle.api.tasks.OutputDirectory
 import org.gradle.api.tasks.TaskAction
